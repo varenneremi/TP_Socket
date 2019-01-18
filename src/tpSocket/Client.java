567663e05d96a23d8b186c4e1dd51b563225d178
LoicSo <souchon.loic@free.fr>
2019-01-18 11:05:09 +0100

    Client baby step

--- src/tpSocket/Client.java
+++ src/tpSocket/Client.java
@@ -1,5 +1,61 @@
 package tpSocket;
 
-public class Client {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+	
+	public static void main(String[] args) {
+		String serverHost = "localHost";
+		int serverPort = 1234;
+		Socket server = null;
+		InputStream is = null;
+		DataInputStream dis = null;
+		OutputStream os = null;
+		DataOutputStream dos = null;
+		
+		try {
+			server = new Socket(serverHost, serverPort);
+		} catch (UnknownHostException e) {
+			System.err.println("Erreur dans le nom de serveur");
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			System.exit(-1);
+		}
+		
+		try {
+			is = server.getInputStream();
+			dis = new DataInputStream(is);
+			
+			os = server.getOutputStream();
+			dos = new DataOutputStream(os);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			System.exit(-1);
+		}
+				
+		String name = "test";
+		
+		try {
+			dos.writeInt(name.length());
+			dos.writeChars(name);
+			
+			int length = dis.readInt();
+			byte[] b = new byte[length];
+			dis.read(b, 0, length);
+			String hello = new String(b);
+			System.out.println(hello);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			System.exit(-1);
+		}
+		
+		
+		
+	}
 }
